9852cf4882f5bb7f07d2b590ec480668f80f685e
Jack Gillan <jcky.g97@gmail.com>
2023-03-01 22:46:27 +0000

    Added app.js as starter node.js web server, modified FE_Obj_Def.py

--- FE AI/FE_Obj_Def.py
+++ FE AI/FE_Obj_Def.py
@@ -2,22 +2,23 @@ class BaseStats:
     def __init__(self, name):
         self.unit_stats = {
             'name' : name,
-            'hp' : 0,
-            'lvl' : 0,
-            'Str' : 0,
-            'Mag' : 0,
-            'Dex' : 0,
-            'Spd' : 0,
-            'Def' : 0,
-            'Res' : 0,
-            'Lck' : 0,
-            'bld' : 0
+            'hp'  : {'base' : 0, 'growth' : 0.0},
+            'lvl' : {'base' : 0, 'growth' : 0.0},
+            'Str' : {'base' : 0, 'growth' : 0.0},
+            'Mag' : {'base' : 0, 'growth' : 0.0},
+            'Dex' : {'base' : 0, 'growth' : 0.0},
+            'Spd' : {'base' : 0, 'growth' : 0.0},
+            'Def' : {'base' : 0, 'growth' : 0.0},
+            'Res' : {'base' : 0, 'growth' : 0.0},
+            'Lck' : {'base' : 0, 'growth' : 0.0},
+            'bld' : {'base' : 0, 'growth' : 0.0}
         }
 
         #Need to assign unit stats at some point, but can bodge in something for testing
         for key in self.unit_stats:
             if key != 'name':
-                self.unit_stats[key] = 1
+                self.unit_stats[key]['base'] = 1
+                self.unit_stats[key]['growth'] = 0.1
 
 unit1 = BaseStats('Alear')
 
