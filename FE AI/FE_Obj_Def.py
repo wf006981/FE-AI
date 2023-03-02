6b1c9fa4316d8e36a6eaa6782a411f2403a91b7a
Jack Gillan <jcky.g97@gmail.com>
2023-03-02 22:22:30 +0000

    modified:   FE AI/FE_Obj_Def.py modified:   FE AI/__pycache__/unit.cpython-311.pyc modified:   FE AI/unit.py new file:   unit_base_stats.csv

--- FE AI/FE_Obj_Def.py
+++ FE AI/FE_Obj_Def.py
@@ -3,4 +3,7 @@ import unit
 unit1 = unit.Unit('Alear')
 
 print(unit1.level_up())
+print(unit1.level_up())
+print(unit1.level_up())
+print(unit1.level_up())
 print(unit1.unit_stats)
--- FE AI/unit.py
+++ FE AI/unit.py
@@ -1,32 +1,43 @@
 import random
+import pandas as pd
 
 class Unit:    
     def __init__(self, name):
+        #Start by initialising stats as a dictionary
+        #Probably going to import this as a JSON eventually but for now this will surfice,
+        #Currently testing on Alear's stats
         self.unit_stats = {
             'name' : name,
             'hp'  : {'current': 0, 'base' : 0, 'growth' : 0.0},
             'lvl' : {'current': 0, 'base' : 1},
-            'Str' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Mag' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Dex' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Spd' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Def' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Res' : {'current': 0, 'base' : 0, 'growth' : 0.0},
-            'Lck' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'str' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'mag' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'dex' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'spd' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'def' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'res' : {'current': 0, 'base' : 0, 'growth' : 0.0},
+            'lck' : {'current': 0, 'base' : 0, 'growth' : 0.0},
             'bld' : {'current': 0, 'base' : 0, 'growth' : 0.0}
         }
 
-        #Need to assign unit stats at some point, but can bodge in something for testing
+        df = pd.read_csv('unit_base_stats.csv')
+        for column in df.columns:
+            for i,row in df.iterrows():
+                if column == 'name':
+                    self.unit_stats[column] = row[column]
+                else:
+                    col_spl = column.split('_')
+                    self.unit_stats[col_spl[0]][col_spl[1]] = row[column] if col_spl[1] == 'base' else (row[column] / 100)
+                
+
+        #Initialise the base, growth, and current rates
         for key in self.unit_stats:
             if key != 'name':
-                self.unit_stats[key]['base'] = 1
-                self.unit_stats[key]['growth'] = 0.5
                 self.unit_stats[key]['current'] = self.unit_stats[key]['base']
 
     def level_up(self):
         lvl_text = ''
-        self.unit_stats['lvl'] = self.unit_stats['lvl']['current'] + 1
-        x= 1 - random.random()
+        self.unit_stats['lvl']['current'] = self.unit_stats['lvl']['current'] + 1
         for key in self.unit_stats:
             if (key not in ['name','lvl']) and ((1 - random.random()) > self.unit_stats[key]['growth']):
                 self.unit_stats[key]['current'] = self.unit_stats[key]['current'] + 1
